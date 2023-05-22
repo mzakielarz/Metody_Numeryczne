10d913c0cb88019cb36b692645e3847abcc2a1fa
Michał Zakielarz <100564500+mzakielarz@users.noreply.github.com>
2023-05-22 11:11:37 +0200

    Add files via upload

---
+++ lab7/main.py
@@ -0,0 +1,25 @@
+import numpy as np
+from scipy.interpolate import lagrange
+
+# mamy 4 punktów w zakresie -1...2 i obliczamy dla nich f(x)
+x1 = np.linspace(-1, 2, 4)
+y1 = [3, 1, 2, -6]
+# wyznaczamy współczynniki
+F = lagrange(x1, y1)
+
+print(f'{F}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
